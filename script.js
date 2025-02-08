94e463513ee94b64a45ced4ead62945517803b92
g0g0-22 <gman89959@gmail.com>
2025-02-08 13:43:42 +0100

    added color selector buttons

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const container = document.querySelector('#container');
 const resetButton = document.getElementById('reset');
-const DIMENSIONS = 16;
+const DIMENSIONS = 64;
 let isDrawing = false;
+let color = 'black';
+
+const colorSelectors = document.querySelectorAll('.colorSelectors div');
+colorSelectors.forEach((selector) => selector.style.backgroundColor = selector.id);
 
 function drawDisplay() {
-    container.innerHTML = '';
+  container.innerHTML = '';
   for (let i = 0; i < DIMENSIONS * DIMENSIONS; i++) {
     const pixel = document.createElement('div');
     pixel.classList.add('pixel');
@@ -12,18 +16,19 @@ function drawDisplay() {
     pixel.style.height = `${100 / DIMENSIONS}%`;
 
     // Start drawing on pointer down
-    pixel.addEventListener('pointerdown', () => {
+    pixel.addEventListener('pointerdown', (e) => {
+      e.preventDefault();
       isDrawing = true;
-      pixel.classList.add('active');
+      pixel.classList.add(color);
     });
 
     // Draw on pointer move if in drawing mode
-    pixel.addEventListener('pointerover', () => {
+    pixel.addEventListener('pointerover', (e) => {
+      e.preventDefault();
       if (isDrawing) {
-        pixel.classList.add('active');
+        pixel.classList.add(color);
       }
     });
-
     container.appendChild(pixel);
   }
 }
