97608185cf21f93aff41d9da4cb4fda1b6326828
g0g0-22 <gman89959@gmail.com>
2025-02-11 00:20:42 +0100

    Added eraser functionality.

--- script.js
+++ script.js
@@ -4,6 +4,41 @@ const DIMENSIONS = 64;
 let isDrawing = false;
 let color = 'black';
 let grid = true;
+let lastpixel = null;
+let activeEraser = false;
+let previousColor = null;
+const eraser = document.getElementById('eraser');
+const eraserTopBox = document.getElementById('light');
+//Toggles the eraser on and off.
+eraser.addEventListener('click', () => {
+  activeEraser = !activeEraser;
+  if(color !== 'white'){
+    previousColor = color;
+  }
+  if(activeEraser) {
+    color = 'white';
+  } else {
+    color = previousColor;
+  }  
+
+  
+  //Deselects all color selectors.
+  if(!activeEraser){
+    const previousColorSelector = document.getElementById(previousColor);
+    previousColorSelector.classList.toggle('active');
+  }
+  else{
+    colorSelectors.forEach((selector) => {
+      selector.classList.remove('active');
+    });
+  }
+  eraser.classList.toggle('activeEraser');
+  eraserTopBox.classList.toggle('activeEraserTopBox');
+  
+});
+
+
+//Toggles pixel borders (grid) on and off.
 const gridToggle = document.getElementById('gridToggle');
 gridToggle.addEventListener('click', () => {
   const gridIcons = gridToggle.querySelectorAll('.gridIconBox');
@@ -16,6 +51,8 @@ gridToggle.addEventListener('click', () => {
   });
   grid = !grid;
 });
+
+// Selects the color of the pixel.
 const colorSelectors = document.querySelectorAll('.colorSelectors div');
 colorSelectors.forEach((selector) => {
   selector.style.backgroundColor = selector.id
@@ -26,10 +63,24 @@ colorSelectors.forEach((selector) => {
     });
     color = selector.id;
     selector.classList.add('active');
+    if(activeEraser) {
+      eraser.classList.toggle('activeEraser');
+      eraserTopBox.classList.toggle('activeEraserTopBox');
+      activeEraser = false;
+    }
   });
-
+  
+  
+  
 });
 
+// Updates the color of the pixel.
+function updatePixelColor(pixel) {
+  pixel.classList.remove('black', 'red', 'green', 'blue', 'yellow', 'purple', 'orange', 'gray');
+  pixel.classList.add(color);
+}
+
+//Draws the display, creates the pixels and adds event listeners.
 function drawDisplay() {
   container.innerHTML = '';
   for (let i = 0; i < DIMENSIONS * DIMENSIONS; i++) {
@@ -37,21 +88,25 @@ function drawDisplay() {
     pixel.classList.add('pixel');
     pixel.style.width = `${100 / DIMENSIONS}%`;
     pixel.style.height = `${100 / DIMENSIONS}%`;
+
     if(!grid) {
       pixel.classList.add('no-grid');
     }
+
     // Start drawing on pointer down
     pixel.addEventListener('pointerdown', (e) => {
       e.preventDefault();
       isDrawing = true;
-      pixel.classList.add(color);
+      lastpixel = pixel;
+      updatePixelColor(pixel);
     });
 
     // Draw on pointer move if in drawing mode
-    pixel.addEventListener('pointerover', (e) => {
+    pixel.addEventListener('pointerenter', (e) => {
       e.preventDefault();
       if (isDrawing) {
-        pixel.classList.add(color);
+        updatePixelColor(pixel);
+        lastpixel = pixel;
       }
     });
     container.appendChild(pixel);
@@ -67,6 +122,5 @@ window.addEventListener('pointerup', () => {
 
 
 drawDisplay();
-
 resetButton.addEventListener('click', drawDisplay);
 
